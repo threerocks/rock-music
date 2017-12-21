00ec3db5f4ad1a404af6879ba4af057bba571039
liulei <liulei@eduise.com>
2017-12-21 16:30:55 +0800

    modified:   src/api/config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,8 @@
 'use strict';
 
-export const host = `http://${window.location.host.replace(/:.*$/, '')}:3000`;
+// export const host = `http://${window.location.host.replace(/:.*$/, '')}:3000`;
+export const host = `http://localhost:3000`;
+
 export const ERR_OK = 200;
 
 export const commonParams = {
